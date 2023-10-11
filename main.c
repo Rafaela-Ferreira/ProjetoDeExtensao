f6ada5eba0122d8ebedeb0876a2f6768201af815
Rafaela Ferreira <102605251+Rafaela-Ferreira@users.noreply.github.com>
2023-10-11 10:51:31 -0300

    Update main.c
    atualização da opção contrato

--- main.c
+++ main.c
@@ -151,65 +151,171 @@ int main() {
         printf("Voc� selecionou Contrato.\n");
         // Coloque a l�gica do contrato aqui
         // Vari�veis para informa��es do contrato
-        char nomeCliente[100];
-        float valorLocacao;
-        char telefoneCliente[20];
-        char dataEvento[20];
-        int quantidadeDias;
-        int diaSemana;
-        // Exibir a sele��o do contrato
-        // Solicitar informa��es para o contrato
-        printf("\nPREENCHA AS INFORMA��ES NECESS�RIAS PARA IMPRESS�O!\n");
-        printf("\nInforme o nome do cliente: ");
-        scanf("%s", nomeCliente);
-    
-        printf("\nInforme o telefone do cliente (ex:(11)99999-8888): ");
-        scanf(" %s", telefoneCliente);
-    
-        printf("\nInforme a data do evento/locacao (DD/MM/AAAA): ");
-        scanf("%s", dataEvento);
-    
-        printf("\nInforme o dia da semana da loca��o: \nDigite 1: SEG - SEX \nDigite 2: SAB - DOM \n");
-        scanf("%d", &diaSemana);
-        //Verfica��o do dia de semana
-        while(diaSemana != 1 && diaSemana != 2){
-          printf("\nERRO! \nDigite 1 = Para dias entre Segunda e Sexta \nDigite 2 = Para S�bado ou Domingo!");
-          printf("\nInforme o dia de semana: ");
-          scanf("%d", &diaSemana);
+    char nomeCliente[100];
+    float valorLocacao = 0.0;
+    int telefoneCliente;
+    int dddTelefone;
+    int diaEvento;
+    int mesEvento;
+    int anoEvento;
+    int anoBissexto;
+    int quantidadeDias;
+    int maxDiasFevereiro = 28;
+    int diaSemana[31];
+    int i;
+
+    // SOLICITAR INFORMA��ES PARA O CONTRATO
+    printf("\nPREENCHA AS INFORMA��ES NECESS�RIAS PARA IMPRESS�O!\n");
+    printf("\nInforme o nome do cliente: ");
+    scanf("%99[^\n]", nomeCliente);
+
+    // 1. DDD TELEFONE DO CLIENTE:
+    printf("\nInforme o ddd do telefone do cliente: ");
+    scanf("%d", &dddTelefone);
+
+    // 1.1 Verifica��o ddd telefone do cliente
+    while(dddTelefone < 11 || dddTelefone > 99)
+        {
+        printf("\nERRO! o ddd digitado � inv�lido!");
+        printf("\nDigite um ddd v�lido: ");
+        scanf("%d", &dddTelefone);
         }
 
-        printf("\nInforme a quantidade de dias de locacao: ");
+    //2. TELEFONE DO CLIENTE:
+    printf("\nInforme o telefone do cliente: ");
+    scanf("%d", &telefoneCliente);
+
+    // 2.1 Verifica��o Telefone do cliente
+    while(telefoneCliente < 0)
+        {
+        printf("\nERRO! o valor digitado � inv�lido!");
+        printf("\nDigite um telefone v�lido: ");
+        scanf("%d", &telefoneCliente);
+        }
+
+    // 3. ANO DA LOCA��O:
+    printf("\nInforme o ano do evento/loca��o: ");
+    scanf("%d", &anoEvento);
+
+    // 3.1 Verifica��o ano da loca��o
+    while(anoEvento < 0 || anoEvento < 2023){
+        printf("\nERRO! Ano do evento digitado inv�lido!");
+        printf("\nDigite novamente o ano do evento: ");
+        scanf("%d", &anoEvento);
+    }
+
+    // 3. M�S DA LOCA��O:
+    printf("\nInforme o m�s do evento/loca��o: ");
+    scanf("%d", &mesEvento);
+
+    // 3.1 Verifica��o m�s da loca��o
+    while(mesEvento <=0 || mesEvento > 12)
+        {
+        printf("\nERRO! M�s do evento digitado inv�lido!");
+        printf("\nDigite novamente o m�s do evento: ");
+        scanf("%d", &mesEvento);
+        }
+
+
+    // 4. DIA DA LOCA��O:
+    printf("\nInforme o dia do evento/locacao: ");
+    scanf("%d", &diaEvento);
+
+    // 4.1 Verifica��o dia da loca��o (Meses com 31 dias)
+    if(mesEvento == 1 || mesEvento == 3 || mesEvento == 5 || mesEvento == 7 || mesEvento == 8 || mesEvento == 10 || mesEvento == 12)
+    {
+    while(diaEvento < 0 || diaEvento > 31)
+        {
+        printf("\nERRO! Dia do evento digitado inv�lido!");
+        printf("\nDigite novamente dia do evento (1 - 31): ");
+        scanf("%d", &diaEvento);
+        }
+    }
+
+    // 4.2 Verifica��o dia da loca��o (M�s com 28 ou 29 dias)
+    // 4.3 verificar se o ano � bissexto ou n�o
+    for (int anoBissexto = 2024; anoBissexto <= anoEvento; anoBissexto += 4)
+    {
+        maxDiasFevereiro = (anoBissexto % 4 == 0 && (anoBissexto % 100 != 0 || anoBissexto % 400 == 0)) ? 29 : 28;
+    }
+
+    if (mesEvento == 2)
+    {
+        while (diaEvento < 1 || diaEvento > maxDiasFevereiro)
+        {
+            printf("\nERRO! Dia do evento digitado inv�lido para fevereiro em um ano %s bissexto!\n", (maxDiasFevereiro == 29) ? "�" : "n�o �");
+            printf("Digite novamente o dia do evento (1 - %d): ", maxDiasFevereiro);
+            scanf("%d", &diaEvento);
+        }
+    }
+
+    // 4.4 Verificando dia da loca��o (M�s com 30 dias)
+    else if(mesEvento == 4 || mesEvento == 6 || mesEvento == 9 || mesEvento == 11)
+    {
+        while(diaEvento < 0 || diaEvento > 30)
+        {
+        printf("\nERRO! Dia do evento digitado inv�lido!");
+        printf("\nDigite novamente dia do evento (1 - 30): ");
+        scanf("%d", &diaEvento);
+        }
+    }
+
+     // 5. QUANTIDADE DE DIAS LOCA��O
+    printf("\nInforme a quantidade de dias de locacao: ");
+    scanf("%d", &quantidadeDias);
+
+    // 5.1 Verifica��o da quantidade de dias
+    while(quantidadeDias < 1 || quantidadeDias > 31)
+    {
+        printf("\nERRO! O valor digitado � inv�lido ou excede o limite m�ximo de 31 dias!");
+        printf("\nDigite a quantidade de dias novamente: ");
         scanf("%d", &quantidadeDias);
-        //Verifica��o da quantidade de dias
-        while(quantidadeDias < 1){
-          printf("\nERRO! O valor digitado � inv�lido!");
-          printf("\nDigite um valor v�lido: ");
-          scanf("%d", &quantidadeDias);
-        }
+    }
 
-        //Calculando valor total de loca��o
-        if(diaSemana == 1){
-            valorLocacao = 500 * quantidadeDias;
-        }
-        else if(diaSemana == 2){
-            valorLocacao = 550 * quantidadeDias;
-        }
+    // 6. DIAS DA LOCA��O
+    for(i = 1; i <= quantidadeDias; i++)
+    {
+        printf("\nInforme o %d� dia da semana da loca��o \n1 - Segunda \n2 - Ter�a \n3 - Quarta \n4 - Quinta \n5 - Sexta \n6 - S�bado \n7 - Domingo: ", i);
+        scanf("%d", &diaSemana[i -1]);
+    }
 
-        // Imprimir os dados no sistema
-        printf("\n\n\nCONTRATO DE LOCA��O RECANTO DOS SONHOS\n\n");
-        printf("Valor da diaria: Segunda � Sexta: R$500,00 - Sabado � Domingo: R$550,00\n");
-        printf("Telefone: (19) 99580-8156\n");
-        printf("Endereco: Rua dois, N� 1071 - jardim Itagua�u 2, Campinas - SP, CEP 13053-788, Brasil\n\n");
-        printf("Locatario: %s\n", nomeCliente);
-        printf("Quantidade de Dias de Locacao: %d\n", quantidadeDias);
-        printf("Valor total da Locacao: R$%.2f\n", valorLocacao);
-        printf("Telefone do Locatario: %s\n", telefoneCliente);
-        printf("Data do Evento/Locacao: %s\n\n", dataEvento);
-        printf("Assinatura do Locatario: _____\n");
-        printf("Data de hoje: _ /_ /___\n");
-        printf("Campinas, SP\n\n");
-        printf("Assinatura do Locador: _____\n");
+    // 7. VALOR DA LOCA��O (QUANTIDADE DE DIAS E DIAS DE SEMANA)
+    for(i = 0; i < quantidadeDias; i++)
+    {
+        switch (diaSemana[i]) {
+            case 1: // Segunda
+            case 2: // Ter�a
+            case 3: // Quarta
+            case 4: // Quinta
+            case 5: // Sexta
+                valorLocacao += 500.0;
+                break;
+            case 6: // S�bado
+            case 7: // Domingo
+                valorLocacao += 550.0;
+                break;
+            default:
+                printf("\nERRO! Dia da semana inv�lido! O dia %d n�o ser� considerado.\n", diaSemana[i]);
+                break;
+        }
+    }
 
+    // IMPRIMIR DADOS NA TELA DO SISTEMA (obs: impress�o final ser� feita em txt)
+    printf("\n\n\nCONTRATO DE LOCA��O RECANTO DOS SONHOS\n\n");
+    printf("Valor da diaria: Segunda � Sexta: R$500,00 - Sabado � Domingo: R$550,00\n");
+    printf("Telefone: (19) 99580-8156\n");
+    printf("Endereco: Rua dois, N� 1071 - jardim Itagua�u 2, Campinas - SP, CEP 13053-788, Brasil\n\n");
+    printf("Locatario: %s\n", nomeCliente);
+    printf("Quantidade de Dias de Locacao: %d\n", quantidadeDias);
+    printf("Valor total da Loca��o: R$%.2f\n", valorLocacao);
+    printf("Telefone do Locatario: (%d)%d\n", dddTelefone, telefoneCliente);
+    printf("Data do Evento/Locacao: %d/%d/%d\n\n", diaEvento, mesEvento, anoEvento);
+    printf("Assinatura do Locatario: ____________\n");
+    printf("Data de hoje: __ /__ /_____\n");
+    printf("Campinas, SP\n\n");
+    printf("Assinatura do Locador: ____________\n");
+
+    
         return 0;
         break;
       case 5:
